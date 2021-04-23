698d1a5355fc481f3de0c3ed125cd05986a738db
BiKunTin <cuong.hsgs@gmail.com>
2021-04-23 12:47:04 -0500

    deploys

---
+++ src/main/resources/static/precache-manifest.ecba2d2cda056d86f7e3b026f171c21b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ab13e3338e9c27cbe10856304878ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42908cc4d285570c7727",
+    "url": "/static/css/2.45813674.chunk.css"
+  },
+  {
+    "revision": "22c13bed156aba41401f",
+    "url": "/static/css/main.199d218b.chunk.css"
+  },
+  {
+    "revision": "42908cc4d285570c7727",
+    "url": "/static/js/2.15b72bf7.chunk.js"
+  },
+  {
+    "revision": "147d8de48b3215786dee8cda9b455378",
+    "url": "/static/js/2.15b72bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c13bed156aba41401f",
+    "url": "/static/js/main.db4d7a19.chunk.js"
+  },
+  {
+    "revision": "153c536528ca7840c2ab",
+    "url": "/static/js/runtime-main.98de0f99.js"
+  },
+  {
+    "revision": "e474c70b5b28afa49a60b34022859583",
+    "url": "/static/media/Coronavirus-EN-2.e474c70b.png"
+  },
+  {
+    "revision": "94a835285e2942ebc5e086a3413c315a",
+    "url": "/static/media/be-kind-to-support.94a83528.jpg"
+  },
+  {
+    "revision": "e57941508c1ddb3fb3f8ebc1b66a5d8c",
+    "url": "/static/media/bob_covid_large.e5794150.jpg"
+  },
+  {
+    "revision": "bbf0874e8cd3c24f025d8f4085ea7739",
+    "url": "/static/media/cartoon.bbf0874e.jpg"
+  },
+  {
+    "revision": "dcd1ed41adaa904d5a9de6cec81afa76",
+    "url": "/static/media/cuong.dcd1ed41.png"
+  },
+  {
+    "revision": "2225eb6119f85a2e466bad1e36577603",
+    "url": "/static/media/damon.2225eb61.png"
+  },
+  {
+    "revision": "562f272a054b9e426805dfdd9b0df419",
+    "url": "/static/media/download.562f272a.jfif"
+  },
+  {
+    "revision": "4117439028601c000e3b3e392b114f84",
+    "url": "/static/media/joe.41174390.png"
+  },
+  {
+    "revision": "ba8d04b19af75e336813df4e0f37089f",
+    "url": "/static/media/nick.ba8d04b1.png"
+  },
+  {
+    "revision": "8708f80808eead3e954ab5db0b082890",
+    "url": "/static/media/original.8708f808.jfif"
+  },
+  {
+    "revision": "3025edd713927c93e4e609a122558a4f",
+    "url": "/static/media/social-distancing-together-6-feet.3025edd7.webp"
+  },
+  {
+    "revision": "6770b4bbe38740654d8e5b300efac01a",
+    "url": "/static/media/spread.6770b4bb.png"
+  },
+  {
+    "revision": "003861869df2008f5ec4ebfa2b27cd9d",
+    "url": "/static/media/touch.00386186.jpg"
+  },
+  {
+    "revision": "4511bc8ed10b3084b4b45654c000f239",
+    "url": "/static/media/zach.4511bc8e.png"
+  }
+]);
